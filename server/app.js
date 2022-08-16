b94c832649d572f9e90c5768c2750ec159cf7afb
Gunwoo.Heo <kw.hur@exinno.net>
2022-08-16 15:14:53 +0900

    rename  route -> controller

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var apiRouter = require('./routes/api');
+var indexRouter = require('./controller/index');
+var usersRouter = require('./controller/users');
+var apiRouter = require('./controller/api');
 
 var app = express();
 
